511de0b78eefb411a7e0316a43682325921c540f
ych1997 <ac30221@gmail.com>
2021-05-24 15:14:42 +0800

    Add files via upload

--- hw3-1.py
+++ hw3-1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-img = cv.imread('1.png')
-mask = cv.imread('1.png',0)
+img = cv.imread('einsteinSample.jpg')
+mask = cv.imread('einsteinMask.jpg',0)
 dst = cv.inpaint(img,mask,3,cv.INPAINT_TELEA)
-cv.imwrite('1_result.jpg',dst)
+cv.imwrite("result2.jpg",dst)
